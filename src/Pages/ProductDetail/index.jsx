6bb945e3872ac7aabc0b3c238cc7a8c72c00f877
GulievKamran <kamranquliyev7778@gmail.com>
2025-03-17 04:50:24 -0700

    encommerse1

--- src/Pages/ProductDetail/index.jsx
+++ src/Pages/ProductDetail/index.jsx
@@ -2,18 +2,25 @@ import React, { useEffect, useState } from 'react'
 import Navbar from '../../components/Navbar'
 import { useParams } from 'react-router';
 import axios from 'axios';
+import Rating from '../../components/Rating';
+
+
+
+
+
 
 const ProductDetail = () => {
   const {slug} = useParams();
   const [product,setProduct]=useState({})
   const url="http://localhost:3000/products";
+  const [currentImage,setCurrentImage]=useState("");
 
   useEffect(()=>{
     axios.get(url).then(({data})=>{
       const currentElement= data.find((e)=> e.slug ===slug);
     console.log(currentElement);
          setProduct(currentElement);
-      
+          setCurrentImage(currentElement.category?.image)
     });
   },[]);
   
@@ -22,20 +29,21 @@ console.log(product);
   <>
     <Navbar/> 
   
-  <div className="grid grid-cols-2 px-5 gap-25">
-    <div>
- <img src={product.category?.image} className='h-[300px]'/>
-    <div className="grid grid-cols-[100px_100px_100px] gap-0">
-    {product.images?.map(({image})=>{
-     return <img src={image} className='w-[100px]'/>;
+  <div className="grid grid-cols-2 px-5 gap-25 mt-4">
+    <div className='border-2 border-amber-500 p-8 grid grid-cols-[1fr_100px] gap-5'>
+ <img src={currentImage}/> 
+    <div className="grid grid-cols-[100px] grid-rows-[100px_100px_100px] object-cover gap-3 ">
+    {product.images?.map((image)=>{
+     return <img src={image} className='w-full h-full'/>;
     })}
     </div>
     </div>
     <div>
-<h2>{product.title}</h2>
-<h2>{product.description}</h2>
-<h2>{product.price}</h2>
-<h2>{product?.category?.name}</h2>
+<h2 className='text-3xl font-extrabold text-sky-600'>{product.title}</h2>
+<h2 className='my-3 text-2xl'>{product.description}</h2>
+
+<h2 className='text-4xl text-red-700 font-bold'>{product.price} Pound</h2>
+<Rating rating={product.rating} />
     </div>
   </div>
 
